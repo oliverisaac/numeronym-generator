c7691fb1a28f08eec71998803725c2a6787f142f
Oliver Isaac <oisaac@gmail.com>
2019-08-01 13:15:31 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Numeronym Generator
+
+
+Generates numberonyms from STDIN.
+
+
+# Usage:
+
+``` shell
+
+echo "hello world, I have a name" | bash main.sh
+
+```
---
+++ main.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# source code generated using shource: https://github.com/oliverisaac/shource
+# vim: autoindent tabstop=4 shiftwidth=4 expandtab softtabstop=4 ft=sh
+
+set -e # Exit on any error. Use `COMMAND || true` to nullify
+set -E # Functions inherit error trap
+set -u # Error on unset variables. Use ${var:-alternate value} to bypass
+set -f # Error on failed file globs (e.g. *.txt )
+set -o pipefail # Failed commands in pipes cause the whole pipe to fail
+
+export IFS=$'\n\t ,.';
+
+while read line; do
+    for word in $line; do
+       echo "${word:0:1}$(( ${#word} - 2 ))${word: -1}" 
+    done
+done | tr '\n' ' '
